b933568cfc4b2854b383835b74bcad3269f42d8c
John <johnjmartiniv@gmail.com>
2015-03-06 18:17:13 -0500

    Initial Commit

---
+++ caffiend-storyboardTests/caffiend_storyboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  caffiend_storyboardTests.swift
+//  caffiend-storyboardTests
+//
+//  Created by John Martin on 3/6/15.
+//  Copyright (c) 2015 John Martin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class caffiend_storyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
